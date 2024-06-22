835aab02fb99431a9b66ada05cd768f15676b726
Takayuki Moto <takayuki.moto@arsaga.jp>
2024-06-22 16:28:56 +0900

    add202406221620

--- .markuplintrc.mjs
+++ .markuplintrc.mjs
@@ -1,4 +1,4 @@
-export const markuplintConfig = {
+const markuplintConfig = {
   extends: ['markuplint:recommended'],
   overrideMode: 'merge',
   overrides: {
@@ -9,7 +9,7 @@ export const markuplintConfig = {
     },
   },
   // Error: Matching for a tree with var() is not supportedが解決できないためexclude
-  // excludeFiles: ['./src/components/ui/rating-group.tsx'],
+  excludeFiles: ['./src/components/ui/rating-group.tsx'],
   parser: {
     '\\.astro$': '@markuplint/astro-parser',
     '\\.tsx$': '@markuplint/jsx-parser',
@@ -469,4 +469,4 @@ export const markuplintConfig = {
   ],
 }
 
-// export default markuplintConfig
+export default markuplintConfig
