40c694def4d07c8f95956ebacfed537327a14d72
Takayuki Moto <takayuki.moto@arsaga.jp>
2024-08-12 14:38:21 +0900

    add202408121438

--- .textlintrc.js
+++ .textlintrc.js
@@ -6,6 +6,25 @@ module.exports = {
   },
   filters: {},
   rules: {
-    'no-todo': true, // TODO: このルールを削除して、他のルールを追加すること
+    'preset-ja-technical-writing': {
+      'no-exclamation-question-mark': false,
+      'ja-no-mixed-period': {
+        periodMark: ['。', '笑', 'w', 'W', '…', '...'],
+        allowEmojiAtEnd: true,
+      },
+      'sentence-length': {
+        max: 150,
+      },
+      'ja-no-weak-phrase': false,
+    },
+    'no-surrogate-pair': true,
+    'no-mixed-zenkaku-and-hankaku-alphabet': true,
+    'ja-hiragana-keishikimeishi': true,
+    'ja-hiragana-fukushi': true,
+    'ja-hiragana-hojodoushi': true,
+    '@textlint-ja/textlint-rule-no-insert-dropping-sa': true,
+    'prefer-tari-tari': true,
+    '@textlint-ja/no-synonyms': true,
+    'ja-no-orthographic-variants': true,
   },
 }
