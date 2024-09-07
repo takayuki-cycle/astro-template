59e1fce9519dcfe160836815cc75c400572de0c8
Takayuki Moto <takayuki.moto@arsaga.jp>
2024-09-07 19:44:47 +0900

    add202409071944

---
+++ src/templates/blog/page/types.ts
@@ -0,0 +1,13 @@
+import type { CollectionEntry } from 'astro:content'
+
+export interface PageProps {
+  data: CollectionEntry<'blog'>[]
+  total: number
+  size: number
+  currentPage: number
+  lastPage: number
+}
+
+export interface PathNameProps {
+  pathName: string
+}
