4d660a3a43a578060399115a2aa55c96505c1093
Takayuki Moto <takayuki.moto@arsaga.jp>
2024-09-08 11:24:03 +0900

    add202409081124

---
+++ src/templates/blog/slug/types.ts
@@ -0,0 +1,9 @@
+export type SlugProps = {
+  ogImage: string
+  ogImageAlt?: string | undefined
+  title: string
+}
+
+export interface PathNameProps {
+  pathName: string
+}
---
+++ src/templates/blog/slug/utils.ts
@@ -0,0 +1,3 @@
+export interface PathNameProps {
+  pathName: string
+}
