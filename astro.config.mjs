7823ceeb6d0b846fef44d294590dc3fd8bfe4bce
Takayuki Moto <takayuki.moto@arsaga.jp>
2024-09-14 16:04:56 +0900

    add202409141604

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,6 @@ import { defineConfig } from 'astro/config'
 // https://astro.build/config
 export default defineConfig({
   site: 'https://takayukiMoto-arsaga.github.io',
-  base: '/my-repo',
+  base: '/astro-boilerplate',
   integrations: [react(), sitemap(), mdx()],
 })
