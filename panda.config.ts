00155ae3bbc4f5a5cb0c9e6422ca92d2fa816df4
Takayuki Moto <takayuki.moto@arsaga.jp>
2024-09-22 19:42:16 +0900

    add202409221942

--- panda.config.ts
+++ panda.config.ts
@@ -1,39 +1,32 @@
-import { defineConfig } from '@pandacss/dev'
-
 // TODO: 設定を変更したときは、npm run prepareを実行しないと反映されないので注意
+import { defineConfig, defineGlobalStyles } from '@pandacss/dev'
+
+const globalCss = defineGlobalStyles({
+  html: {
+    '--global-font-body': 'Noto Sans JP',
+    // '--global-font-mono': 'Mononoki Nerd Font, monospace',
+    fontOpticalSizing: 'auto',
+  },
+  strong: {
+    fontWeight: 'normal',
+  },
+  textarea: { resize: 'none' },
+  summary: {},
+})
+
 export default defineConfig({
   preflight: true, // リセットCSSを適用
   presets: ['@pandacss/preset-base', '@pandacss/preset-panda', '@park-ui/panda-preset'],
   include: ['./src/**/*.{ts,tsx,js,jsx,astro}', './pages/**/*.{ts,tsx,js,jsx,astro}'],
   exclude: [],
-  validation: 'error',
+  // validation: 'error',
   jsxFramework: 'react',
   // jsxStyleProps: 'none', // TODO: コメントアウトしてビルドしたときに警告が出なければ、この行を使ってください。
-  strictTokens: true, // UIコンポーネントライブラリを使わない選択をするのであれば、この行を使う可能性あり  トークン値だけ使用可能(px, rem, blueなどは使用不可)
+  // strictTokens: true, // UIコンポーネントライブラリを使わない選択をするのであれば、この行を使う可能性あり  トークン値だけ使用可能(px, rem, blueなどは使用不可)
   strictPropertyValues: true,
   outdir: 'styled-system',
-  globalFontface: {
-    /* TODO: フォントを追加する場合は、このコメントアウトを外してください。
-    Inter: {
-      src: "url(/fonts/inter.woff2) format('woff2')",
-      fontWeight: '400',
-      fontStyle: 'normal',
-    },
-    Roboto: {
-      src: "url(/fonts/roboto.woff2) format('woff2')",
-      fontWeight: '400',
-      fontStyle: 'normal',
-    },
-    */
-  },
   theme: {
     extend: {},
   },
-  globalCss: {
-    strong: {
-      fontWeight: 'normal',
-    },
-    textarea: { resize: 'none' },
-    summary: {},
-  },
+  globalCss,
 })
