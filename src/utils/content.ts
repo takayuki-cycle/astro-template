5952a09161514df722440df8b6ed8b7428e9ae78
Takayuki Moto <takayuki.moto@arsaga.jp>
2024-10-12 17:01:24 +0900

    add202410121701

--- src/utils/content.ts
+++ src/utils/content.ts
@@ -1,6 +1,16 @@
 import { getCollection } from 'astro:content'
 import type { GetStaticPathsOptions } from 'astro'
 
+const sortByPublishDateDesc = (
+  a: { data: { publishDate: string } },
+  b: { data: { publishDate: string } },
+) => {
+  const dateA = new Date(a.data.publishDate).getTime()
+  const dateB = new Date(b.data.publishDate).getTime()
+
+  return dateB - dateA
+}
+
 export const getPaginatedPaths = async (
   pathName: 'blog', // TODO: 他のパスを追加する場合は、ここに追加(例: 'blog' | 'authors')
   paginate: GetStaticPathsOptions['paginate'],
@@ -9,7 +19,7 @@ export const getPaginatedPaths = async (
   const posts = await getCollection(pathName, ({ data }) => {
     // 本番用にビルドするときにのみ、isDraft: trueを含むエントリーを除外
     return import.meta.env.PROD ? data.isDraft !== true : true
-  })
+  }).then((collection) => collection.sort(sortByPublishDateDesc))
 
   return paginate(posts, { pageSize: pageSize })
 }
@@ -22,7 +32,7 @@ export const getCategoryPaginatedPaths = async (
   const posts = await getCollection(pathName, ({ data }) => {
     // 本番用にビルドするときにのみ、isDraft: trueを含むエントリーを除外
     return import.meta.env.PROD ? data.isDraft !== true : true
-  })
+  }).then((collection) => collection.sort(sortByPublishDateDesc))
   const category = posts
     .map((post) => post.data.categories)
     .filter((categories): categories is string[] => categories !== undefined)
