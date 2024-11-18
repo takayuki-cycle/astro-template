77ef51520643e86c04cc79fae34f50cb75bbcc64
Takayuki Moto <takayuki.moto@arsaga.jp>
2024-11-18 16:12:09 +0900

    add202411181612

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -80,13 +80,15 @@ const eslintConfig = [
         ]),
         // 特定のHTMLタグの使用を禁止
         // ['使用を禁止したいHTMLタグ', '代わりに提案したい方法'](代わりに提案したい方法がなければ、''にしてください。)
-        // Markuplintで<font>、<center>、<marquee>、<frameset>、<frame>、<noframes>、<big>、<applet>、<blink>の使用は、既に禁止されています。
+        // Markuplintで<font>、<center>、<marquee>、<frameset>、<frame>、<noframes>、<big>、<applet>、<blink>、<strike>、<rb>、<rtc>、<acronym>、<basefont>、<bgsound>、<dir>、<isindex>、<keygen>、<listing>、<marquee>、<menuitem>、<nobr>、<noembed>、<plaintext>、<tt>、<xmp>の使用は、既に禁止されています。
         // <small>は補足情報で使用するので、禁止にはしていません。
         ...createHTMLTagNoRestrictedSyntax([
           ['b', '<strong>または<em>またはスタイルで太字'],
           ['i', '<em>または<cite>またはスタイルで斜体'],
           ['u', '<mark>または<abbr>またはスタイルで下線'],
           ['s', '<del>または<ins>またはスタイルで打ち消し線'],
+          ['hgroup', '<header>または<section>または<div>にARIA属性'],
+          ['command', '<menu>または<button>または<kbd>または<ul>または<ol>または<span>'],
           // ['img', 'Imageコンポーネント'], // TODO: いずれ適用
           // ['style', 'Panda CSS'], // TODO: いずれ適用
         ]),
