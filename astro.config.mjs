a22585b09bac33c97b4bfeff8a09548685d10ea7
Takayuki Moto <b1100770@planet.kanazawa-it.ac.jp>
2024-12-06 18:26:16 +0900

    add202412061826

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import { defineConfig } from 'astro/config'
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://takayuki-cycle.github.io',
+  site: 'https://main.d2869e33y4q9qp.amplifyapp.com',
   trailingSlash: 'always',
   integrations: [react(), sitemap(), mdx()]
 })
