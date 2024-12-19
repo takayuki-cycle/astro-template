3a0bced927894c494f1d08a041becddb8f4dc780
Takayuki Moto <b1100770@planet.kanazawa-it.ac.jp>
2024-12-20 00:38:48 +0900

    add202412200038

--- src/templates/blog/page/types.ts
+++ src/templates/blog/page/types.ts
@@ -1,14 +1,27 @@
 import type { CollectionEntry } from 'astro:content'
 
-export interface PageProps {
-  data: CollectionEntry<'blog'>[]
-  total: number
-  size: number
-  currentPage: number
-  lastPage: number
+export interface Props {
+  pathName: string
+  categoryPathName?: `${string}/${string}`
+  page: {
+    data: CollectionEntry<'blog'>[]
+    total: number
+    size: number
+    currentPage: number
+    lastPage: number
+  }
 }
 
-export interface PathNameProps {
-  pathName: string
-  categoryPathName: `${string}/${string}`
-}
+// TODO: 上記のPropsでエラーが出るようであれば、昔書いた以下のコードへ置き換えてください。
+// export interface PageProps {
+//   data: CollectionEntry<'blog'>[]
+//   total: number
+//   size: number
+//   currentPage: number
+//   lastPage: number
+// }
+
+// export interface PathNameProps {
+//   pathName: string
+//   categoryPathName: `${string}/${string}`
+// }
