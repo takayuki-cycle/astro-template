7d281acf3790a7ca0e5de1fb02405f27a7a16558
Takayuki Moto <b1100770@planet.kanazawa-it.ac.jp>
2025-06-13 23:19:01 +0900

    add202506132318

---
+++ src/assets/icons/index.ts
@@ -0,0 +1,5 @@
+// Reference: https://fonts.google.com/icons
+// 参照先のSVGファイルから上下左右の余白(マージン)を削除しています。
+export { default as SearchIcon } from '@/assets/icons/search.svg'
+export { default as HomeIcon } from '@/assets/icons/home.svg'
+export { default as MenuIcon } from '@/assets/icons/menu.svg'
