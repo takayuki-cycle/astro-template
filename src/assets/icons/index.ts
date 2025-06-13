54d78d5ce12ec398b3fdc53852ed9964584314f2
Takayuki Moto <b1100770@planet.kanazawa-it.ac.jp>
2025-06-13 23:34:14 +0900

    add202506132334

--- src/assets/icons/index.ts
+++ src/assets/icons/index.ts
@@ -2,4 +2,5 @@
 // 参照先のSVGファイルから上下左右の余白(マージン)を削除しています。
 export { default as SearchIcon } from '@/assets/icons/search.svg'
 export { default as HomeIcon } from '@/assets/icons/home.svg'
+export { default as HomeFillIcon } from '@/assets/icons/home-fill.svg'
 export { default as MenuIcon } from '@/assets/icons/menu.svg'
