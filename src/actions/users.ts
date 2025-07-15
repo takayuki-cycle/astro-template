af08df7c2214a8e3530876097c2296bfb2c4a5ee
Takayuki Moto <b1100770@planet.kanazawa-it.ac.jp>
2025-07-15 09:48:35 +0900

    add202507150948

--- src/actions/users.ts
+++ src/actions/users.ts
@@ -1,13 +1,19 @@
-import { defineAction } from 'astro:actions'
+import { defineAction, ActionError } from 'astro:actions'
 import { z } from 'astro:schema'
 
 export const users = {
-  // TODO: getUsersがあいさつするコードになっているので修正が必要
+  // TODO: 勉強中
   getUsers: defineAction({
     input: z.object({
       name: z.string()
     }),
     handler: async (input) => {
+      if (input.name === 'TEST') {
+        throw new ActionError({
+          code: 'NOT_FOUND',
+          message: `サーバは要求された${input.name}のリソースを見つけることができませんでした。`
+        })
+      }
       return `Hello, ${input.name}!`
     }
   }), // actions.users.getUsers()で呼び出し可能
